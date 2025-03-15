252ecf87c922d7321b4e1e577deb49c549d688bb
Brian Leao <brianleao@icloud.com>
2025-03-15 20:17:26 -0300

    first commit with changes

--- devops_challenge.php
+++ devops_challenge.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * @package Devops_challenge_Junior
  * @version 1.0
@@ -10,50 +11,39 @@ Author: Apiki WordPress
 Version: 1.0
 */
 
-$global_lyrics;
-
-function apiki_segura_o_tchan() {
-	$lyrics = $global_lyrics;
-	
-	$lyrics = "Pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Domingo ela não vai (vai, vai)
-	Domingo ela não vai não (vai, vai, vai)
-	Olha, domingo ela não vai (vai, vai)
-	Domingo ela não vai não (vai, vai, vai)
-	O pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Segure o tchan
-	Amare o tchan
-	Segure o tchan tchan tchan tchan
-	Depois de nove meses você vê o resultado
-	Esse é o Gera Samba arrebentando no pedaço
+global $global_lyrics;
+$global_lyrics = "Pau que nasce torto nunca se endireita\nMenina que requebra a mãe pega na cabeça\n
+	Pau que nasce torto nunca se endireita\nMenina que requebra a mãe pega na cabeça\n
+	Domingo ela não vai (vai, vai)\nDomingo ela não vai não (vai, vai, vai)\nOlha, domingo ela não vai (vai, vai)\n
+	Domingo ela não vai não (vai, vai, vai)\nO pau que nasce torto nunca se endireita\nMenina que requebra a mãe pega na cabeça\n
+	Pau que nasce torto nunca se endireita\nMenina que requebra a mãe pega na cabeça\nSegure o tchan\nAmare o tchan\n
+	Segure o tchan tchan tchan tchan\nDepois de nove meses você vê o resultado\nEsse é o Gera Samba arrebentando no pedaço\n
 	Joga ela no meio, mete em cima, mete embaixo";
 
-	$lyrics = explode( "\n", $lyrics )
+function apiki_segura_o_tchan() {
 
-	return wptexturize( $lyrics[ mt_rand( count( $lyrics ) - 1, 0 ) ] );
+	global $global_lyrics;
+	$lyrics = explode( "\n", $global_lyrics );
+
+	return wptexturize( $lyrics[ mt_rand( 0,count( $lyrics ) - 1 ) ] );
 }
 
 function devops_challenge() {
-	$chosen = $undefined_variable;
+	$chosen = apiki_segura_o_tchan();
 	$lang   = '';
 	if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
 		$lang = ' lang="en"';
 	}
 
 	printf(
-		'O texto é: %s %s %s',
-		__( 'Segure o Tchan, by Apiki WordPress:' ),
-		$lang
+		'<p id="devop">%s <span%s>%s</span></p>',
+		esc_html__( 'Segure o Tchan, by Apiki WordPress:', 'devops-challenge' ),
+		$lang,
+		esc_html( $chosen ) 
 	);
 }
 
-add_action( '', 'devops_challenge' );
+add_action( 'admin_notices', 'devops_challenge' );
 
 function devop_css() {
 	echo "
